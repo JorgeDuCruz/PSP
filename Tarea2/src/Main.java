5d1d9194ca5ffa934f58d65f340ba6da62817177
dam <jdurancruz@danielcastelao.org>
2025-09-16 09:40:04 +0200

    tarea 2 hecha

---
+++ Tarea2/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Runtime rt = Runtime.getRuntime();
+        long memoriaLibre=rt.freeMemory(),memoriaTotal=rt.totalMemory(),memoriaUso=memoriaTotal-memoriaLibre;
+        memoriaLibre = memoriaLibre/1024;//Pasa la memoria a KiB
+        memoriaLibre = memoriaLibre/1024;//Pasa la memoria a MiB
+
+        memoriaTotal = memoriaTotal/1024;//Pasa la memoria a KiB
+        memoriaTotal = memoriaTotal/1024;//Pasa la memoria a MiB
+
+        memoriaUso = memoriaUso/1024;//Pasa la memoria a KiB
+        memoriaUso = memoriaUso/1024;//Pasa la memoria a MiB
+
+        System.out.println("Memoria libre: "+memoriaLibre+" Mib\nMemoria total: "+memoriaTotal+" Mib\nMemoria en uso: "+memoriaUso+" Mib");
+        System.out.println("Numero de procesadores: "+rt.availableProcessors());
+        System.out.println(System.getProperties()); // Propiedades de la clase system
+    }
+}
