fe39516fb1b40ddb62ff7d581c335d1b4a271d0f
dam <jdurancruz@danielcastelao.org>
2025-09-24 10:03:55 +0200

    Tercer objetivo cumplido falta hacer que no se reescirban los log

--- Tarea6/src/Lanzador.java
+++ Tarea6/src/Lanzador.java
@@ -4,31 +4,12 @@ public class Lanzador {
 
     public static void LanzarPing(String host){
         ProcessBuilder Ping = new ProcessBuilder("ping",host,"-c","4");
-        InputStream consola;
-        String extra;
+        Ping.redirectOutput(new File("ping_output.log"));
+        Ping.redirectError(new File("ping_Error.log"));
 
         try {
             Process pPing = Ping.start();
             int exi = pPing.waitFor();
-
-
-            if (exi==0){
-                consola = pPing.getInputStream();
-                extra = "[OK]";
-            }
-            else {
-                consola = pPing.getErrorStream();
-                extra = "[ERROR]";
-            }
-            BufferedReader lectura = new BufferedReader(new InputStreamReader(consola));
-
-            while (true) {
-                String linea = lectura.readLine();
-                if (linea==null){
-                    break;
-                }
-                System.out.println(extra+linea);
-            }
 
             System.out.println("Opercaion Completada. Codigo de salida: "+exi);
 
