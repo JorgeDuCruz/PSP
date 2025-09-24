4819bca90dc38de71c40d00f7bcc7fca4955a9af
dam <jdurancruz@danielcastelao.org>
2025-09-24 10:15:10 +0200

    Documentadas las Clases

--- Tarea6/src/Interfaz.java
+++ Tarea6/src/Interfaz.java
@@ -2,15 +2,22 @@ import java.util.Scanner;
 
 public class Interfaz {
     public static void main(String[] args){
+        empezar();
+    }
+
+    /**
+     * Funcion para mostrar la interfaz para elegir el host al que hacer ping hasta que se decida por salir
+     */
+    public static void empezar(){
         String host;
         Scanner sc = new Scanner(System.in);
-        while (true) {
-            System.out.println("Intorduce el Host o IP (o \"salir\" para terminar):");
-            host = sc.nextLine();
-            if (host.equalsIgnoreCase("salir")){
+        while (true) { // bucle infinito que solo sale cuando se cumple la condicion dentro de el
+            System.out.println("Intorduce el Host o IP (o \"salir\" para terminar):"); // Pregunta por la IP o el Host
+            host = sc.nextLine(); //Recoje la direeccion IP o el host al que hacer ping
+            if (host.equalsIgnoreCase("salir")){ // Si escribe salir cumple la condicion de salida del bucle
                 break;
             }
-            Lanzador.LanzarPing(host);
+            Lanzador.LanzarPing(host); // Lanza el comando ping con el host o IP preguntado antes
         }
     }
 }
--- Tarea6/src/Lanzador.java
+++ Tarea6/src/Lanzador.java
@@ -2,19 +2,23 @@ import java.io.*;
 
 public class Lanzador {
 
+    /**
+     * Funcion que lanza el comando Ping a un host recibido con 4 paquetes
+     * @param host IP o nombre del Host al que hacer ping
+     */
     public static void LanzarPing(String host){
-        ProcessBuilder Ping = new ProcessBuilder("ping",host,"-c","4");
-        Ping.redirectOutput(new File("ping_output.log"));
-        Ping.redirectError(new File("ping_Error.log"));
+        ProcessBuilder Ping = new ProcessBuilder("ping",host,"-c","4"); // Prepara el proceso de ejecutar el ping con el host o IP reciibidos
+        Ping.redirectOutput(new File("ping_output.log")); // Redirige la salida del ping a un fichero
+        Ping.redirectError(new File("ping_Error.log")); // Redirige la salida de errores a un fichero
 
         try {
-            Process pPing = Ping.start();
-            int exi = pPing.waitFor();
+            Process pPing = Ping.start(); // Inicia el proceso de Ping
+            int exi = pPing.waitFor(); // Espera a que termine y recoje su codigo de salida
 
-            System.out.println("Opercaion Completada. Codigo de salida: "+exi);
+            System.out.println("Opercaion Completada. Codigo de salida: "+exi); // Muestra su codigo de salida
 
 
-        }catch (IOException e){
+        }catch (IOException e){ // Tratamiento de excepciones
             System.out.println("Error al iniciar proceso: "+e.getMessage());
         } catch (InterruptedException e) {
             System.out.println("Interrumpido proceso: "+e.getMessage());
