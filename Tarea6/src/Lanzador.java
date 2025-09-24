419ba3dfbcfec14573a6435a7bcf3048690dc70a
dam <jdurancruz@danielcastelao.org>
2025-09-24 09:37:37 +0200

    Segundo objetivo cumplido

--- Tarea6/src/Lanzador.java
+++ Tarea6/src/Lanzador.java
@@ -1,16 +1,38 @@
-import java.io.IOException;
+import java.io.*;
 
 public class Lanzador {
+
     public static void LanzarPing(String host){
         ProcessBuilder Ping = new ProcessBuilder("ping",host,"-c","4");
+        InputStream consola;
+        String extra;
 
         try {
-            Ping.inheritIO();
-
             Process pPing = Ping.start();
-            Thread.sleep(3500);
-            int exi = pPing.exitValue();
-            System.out.println("Opercaion Completada: "+exi);
+            int exi = pPing.waitFor();
+
+
+            if (exi==0){
+                consola = pPing.getInputStream();
+                extra = "[OK]";
+            }
+            else {
+                consola = pPing.getErrorStream();
+                extra = "[ERROR]";
+            }
+            BufferedReader lectura = new BufferedReader(new InputStreamReader(consola));
+
+            while (true) {
+                String linea = lectura.readLine();
+                if (linea==null){
+                    break;
+                }
+                System.out.println(extra+linea);
+            }
+
+            System.out.println("Opercaion Completada. Codigo de salida: "+exi);
+
+
         }catch (IOException e){
             System.out.println("Error al iniciar proceso: "+e.getMessage());
         } catch (InterruptedException e) {
