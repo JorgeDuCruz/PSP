10298b2074d6cbce58824ad5d6fd82cf3ca65941
dam <jdurancruz@danielcastelao.org>
2025-10-01 09:47:53 +0200

    Parte3 Tarea 11

--- Tarea11/src/Main.java
+++ Tarea11/src/Main.java
@@ -2,7 +2,20 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Thread hiloInicio = new HilosDomino(1,15);
+        long tiempoInicio = System.currentTimeMillis(),tiempoFin,tiempoEjecucion;
+        Thread hiloInicio = new HilosDomino(1,5);
         hiloInicio.start();
+        while (hiloInicio.isAlive()){
+            System.out.println("[Control Central] Vigilando a Hilo-1... sigue activo.");
+            try {
+                Thread.sleep(200);
+            }catch (InterruptedException e ){
+                System.out.println("Interrumpida espera de main "+e.getMessage());
+            }
+        }
+        System.out.println("[Control Central] Hilo-1 ha terminado.");
+        tiempoFin = System.currentTimeMillis();
+        tiempoEjecucion = tiempoFin-tiempoInicio;
+        System.out.println("Tiempo total de la caída: "+tiempoEjecucion+" ms");
     }
 }
