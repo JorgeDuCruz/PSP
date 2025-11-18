d1d6c49ea53960e53e0c9759e3127c1d64429204
jorge <jdurancruz@danielcastelao.org>
2025-11-18 09:31:15 +0100

    Tarea 31 Hecha

---
+++ Tarea31/src/ClienteDatagrama.java
@@ -0,0 +1,55 @@
+import java.io.IOException;
+import java.net.*;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ClienteDatagrama {
+
+    public static void main(String[] args) {
+
+        int puerto_servidor = 6666;
+        byte[] buffer = new byte[1024];
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            InetAddress direccionServidor = InetAddress.getByName("localhost");
+
+            // Se crea el DatagramSocket sin especificar un puerto, lo que hace que el sistema asigne un puerto aleatorio.
+            // Esto es suficiente para recibir datagramas, ya que el servidor enviará los datos al puerto en el que el cliente está escuchando.
+            DatagramSocket datagramSocket = new DatagramSocket();
+            DatagramPacket peticion = new DatagramPacket(buffer, buffer.length);
+
+            for (int i = 0;i<3;i++)  {
+
+                System.out.println("Escribe el mensaje al servidor");
+                String msj = sc.nextLine();
+                buffer = msj.getBytes();
+
+                DatagramPacket pregunta = new DatagramPacket(buffer, buffer.length, direccionServidor, puerto_servidor);
+                datagramSocket.send(pregunta);
+                System.out.println("Enviado el mensaje al servidor");
+
+                // Se crea un DatagramPacket con el tamaño de buffer especificado.
+                // Este paquete se utilizará para recibir los datos enviados por el cliente.
+
+                // Al ejecutar este método, el buffer se llena con el mensaje enviado por el cliente.
+                // Este método BLOQUEA la ejecución del código hasta que se reciba el paquete completo.
+                // Si el mensaje enviado es más grande que el tamaño del buffer, el mensaje se truncará.
+                datagramSocket.receive(peticion);
+
+                System.out.println("Recibo msj del servidor");
+
+                // Convierte el contenido del buffer recibido en un String para poder procesarlo.
+                String msjServidor = new String(peticion.getData(),0, peticion.getLength());
+                System.out.println("msjServidor = " + msjServidor);
+            }
+        } catch (SocketException ex) {
+            System.out.println("Error con el socket(cliente): "+ ex.getMessage());
+        } catch (UnknownHostException ex) {
+            Logger.getLogger(ClienteDatagrama.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(ClienteDatagrama.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
