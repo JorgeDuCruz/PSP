b99f674d836dd762e06f876c58e0d88463cbb305
jorge <jdurancruz@danielcastelao.org>
2026-01-13 09:45:01 +0100

    Tarea 39 primera parte hecha, falta espedificar contraseña

---
+++ Tarea39/src/Main.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int opcion;
+        do {
+            System.out.println("""
+                    Elige una opción
+                    1. Auditoría de Hash
+                    2. Decodificador César
+                    3. Fuerza Bruta Paralela
+                    0. Salir""");
+            opcion = sc.nextInt();
+            switch (opcion) {
+                case 1: {
+                    comprobarHash("Jueves");
+                    break;
+                }
+                default:{
+                    opcion = 0;
+                }
+            }
+        }while (opcion!=0);
+    }
+
+    public static boolean comprobarHash(String contrasena){
+        try {
+            BufferedReader lector = new BufferedReader(new FileReader("src/diccionario.txt"));
+            String linea = lector.readLine();
+            while (linea != null){
+                if (linea.equals(contrasena)){
+                    System.out.println("¡CONTRASEÑA ENCONTRADA! La clave es: "+linea);
+                    return true;
+                }
+                linea = lector.readLine();
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Fichero no encontrado. "+e.getMessage());
+        } catch (IOException e) {
+            System.out.println("Error al leer la linea. "+e.getMessage());
+        }
+        return false;
+    }
+}
