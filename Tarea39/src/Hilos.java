78c92d4885a0c3edce7a79e483c36fa204f2f7b7
jorge <jdurancruz@danielcastelao.org>
2026-01-14 10:10:20 +0100

    Tarea 39 tercera parte empezada, hay que revisar todo el codigo porque  vaya desastre

--- Tarea39/src/Hilos.java
+++ Tarea39/src/Hilos.java
@@ -1,3 +1,8 @@
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.HexFormat;
+import java.util.Scanner;
+
 public class Hilos extends Thread{
     private Hilos[] hilos;
     private String hash;
@@ -15,10 +20,45 @@ public class Hilos extends Thread{
 
     @Override
     public void run(){
-        char letra = letras[0];
-        while (true){
-            letra = (char) (letra+1);
-
+        String alfabeto = "abcdefghijklmnñopqrstuvwxyz",pruebas,hp;
+        char[] alf = alfabeto.toCharArray();
+        for (char letra:letras){
+            pruebas = ""+letra;
+            for (char l:alf){
+                pruebas = pruebas+l;
+                for (char j:alf){
+                    pruebas = pruebas+j;
+                    for (char p:alf){
+                        pruebas = pruebas+p;
+                        hp = hashear(pruebas);
+                        if (hash.equals(hp)){
+                            System.out.println("Contraseña es "+pruebas);
+                            for (Hilos h:hilos){
+                                h.interrupt();
+                            }
+                        }
+                        else pruebas = pruebas+"\b";
+                    }
+                    pruebas = pruebas+"\b";
+                }
+                pruebas = pruebas+"\b";
+            }
         }
+    }
+
+    private String hashear(String texto){
+        String has;
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+
+            md.update(texto.getBytes());
+            has = HexFormat.of().formatHex(md.digest());
+            return has;
+
+
+        } catch (NoSuchAlgorithmException e) {
+            System.out.println("Erro con el algoritmo de hashing "+e.getMessage());
+        }
+        return null;
     }
 }
